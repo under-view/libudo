8b4f9792bdb4aee8501af4b6821705bad6b69693
Vincent Davis Jr <vince@underview.tech>
2026-01-01 19:39:00 -0500

    tpool: add initial create and destroy funcs
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- src/tpool.c
+++ src/tpool.c
@@ -1,1 +1,94 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#include "log.h"
 #include "tpool.h"
+
+/*
+ * @brief Structure defining the udo_tpool instance.
+ *
+ * @member err  - Stores information about the error that occured
+ *                for the given instance and may later be retrieved
+ *                by caller.
+ * @member free - If structure allocated with calloc(3) member will be
+ *                set to true so that, we know to call free(3) when
+ *                destroying the instance.
+ */
+struct udo_tpool
+{
+	struct udo_log_error_struct err;
+	bool                        free;
+};
+
+
+/***************************************
+ * Start of udo_tpool_create functions *
+ ***************************************/
+
+struct udo_tpool *
+udo_tpool_create (struct udo_tpool *p_tpool,
+                  const void *p_tpool_info)
+{
+	struct udo_tpool *tpool = p_tpool;
+	const struct udo_tpool_create_info *tpool_info = p_tpool_info;
+
+	if (!tpool_info) {
+		udo_log_error("Incorrect data passed\n");
+		return NULL;
+	}
+
+	if (!tpool) {
+		tpool = calloc(1, sizeof(struct udo_tpool));
+		if (!tpool) {
+			udo_log_error("calloc: %s\n", strerror(errno));
+			return NULL;
+		}
+
+		tpool->free = true;
+	}
+
+	return tpool;
+}
+
+/*************************************
+ * End of udo_tpool_create functions *
+ *************************************/
+
+
+/****************************************
+ * Start of udo_tpool_destroy functions *
+ ****************************************/
+
+void
+udo_tpool_destroy (struct udo_tpool *tpool)
+{
+	if (!tpool)
+		return;
+
+	if (tpool->free) {
+		free(tpool);
+	} else {
+		memset(tpool, 0, sizeof(struct udo_tpool));
+	}
+}
+
+/**************************************
+ * End of udo_tpool_destroy functions *
+ **************************************/
+
+
+/*************************************************
+ * Start of non struct udo_tpool param functions *
+ *************************************************/
+
+int
+udo_tpool_get_sizeof (void)
+{
+	return sizeof(struct udo_tpool);
+}
+
+/***********************************************
+ * End of non struct udo_tpool param functions *
+ ***********************************************/
--- tests/test-tpool.c
+++ tests/test-tpool.c
@@ -13,10 +13,56 @@
 #include "macros.h"
 #include "tpool.h"
 
+/****************************************
+ * Start of test_tpool_create functions *
+ ****************************************/
+
+static void UDO_UNUSED
+test_tpool_create (void UDO_UNUSED **state)
+{
+	struct udo_tpool *tpool = NULL;
+
+	struct udo_tpool_create_info tpool_info;
+	memset(&tpool_info, 0, sizeof(tpool_info));
+
+	tpool = udo_tpool_create(NULL, NULL);
+	assert_null(tpool);
+
+	tpool_info.thread_count = 2;
+	tpool_info.queue_size   = UDO_PAGE_SIZE;
+	tpool = udo_tpool_create(NULL, &tpool_info);
+	assert_non_null(tpool);
+
+	udo_tpool_destroy(tpool);
+}
+
+/**************************************
+ * End of test_tpool_create functions *
+ **************************************/
+
+
+/********************************************
+ * Start of test_tpool_get_sizeof functions *
+ ********************************************/
+
+static void UDO_UNUSED
+test_tpool_get_sizeof (void UDO_UNUSED **state)
+{
+	int size = 0;
+	size = udo_tpool_get_sizeof();
+	assert_int_not_equal(size, 0);
+}
+
+/******************************************
+ * End of test_tpool_get_sizeof functions *
+ ******************************************/
+
 int
 main (void)
 {
 	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_tpool_create),
+		cmocka_unit_test(test_tpool_get_sizeof),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
